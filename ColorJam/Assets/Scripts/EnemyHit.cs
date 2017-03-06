81b33b0bd8cfe938146a3b7e2c67c4dc53aa14b3
HappiGF <lggufi@gmail.com>
2017-03-06 18:23:48 -0500

    Unity project
    entire project so far, first commit

---
+++ ColorJam/Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col) { 
+		if (col.gameObject.name == "EnemyRed" && this.name == "projectileRed(Clone)")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.name == "EnemyBlue" && this.name == "projectileBlue(Clone)")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.name == "EnemyYellow" && this.name == "projectileYellow(Clone)")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ ColorJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;
+
+	Vector3 movement;
+	Rigidbody playerRigidbody;
+
+	void Awake()
+	{
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Turning ();
+	}
+
+	void Move (float h, float v)
+	{
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+    void Turning()
+    {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist = 0.0f;
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+        }
+    }
+}
