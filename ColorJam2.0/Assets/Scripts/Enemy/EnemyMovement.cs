61da30036583d6922b9564c122c0798dac4d8f62
Lucas Guzman-Finn <lggufi@gmail.com>
2017-03-10 12:47:10 -0500

    wowooowowowoowowowo

---
+++ ColorJam2.0/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public GameObject Player;
+    public float Speed;
+    public float minDist;
+	public float attackRate;
+
+	void Start()
+	{
+		InvokeRepeating("AttackPlayer", 0.0f, attackRate);
+	}
+
+    void Update()
+    {
+		MoveToPlayer ();
+    }
+
+	void MoveToPlayer()
+	{
+		transform.LookAt(Player.transform);
+
+		if (Vector3.Distance(transform.position, Player.transform.position) >= minDist)
+		{
+
+			transform.position += transform.forward * Speed * Time.deltaTime;
+
+		}
+	}
+
+	void AttackPlayer()
+	{
+		if (Vector3.Distance (transform.position, Player.transform.position) < minDist + 0.01f) 
+		{
+			Debug.Log ("player was hit");
+			Player.GetComponent<PlayerHealth> ().PlayerHit (10);
+		}
+	}
+}
---
+++ ColorJam2.0/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float maxHealth;
+    public float currentHealth;
+
+	void Start () {
+        currentHealth = maxHealth;	
+	}
+	
+	public void PlayerHit(float damg)
+    {
+        currentHealth -= damg;
+        Debug.Log(currentHealth);
+    }
+}
